d963caf8999fe704069324f5658528ae620f4ce4
frankmata17 <frank.mata17@gmail.com>
2025-03-08 15:14:16 -0700

    update server.js

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("🔥 MongoDB connected!"))
-    .catch(err => console.error("❌ MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("🔥 MongoDB connected!"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
+
 
 // Import and use the contacts route
 const contactsRoute = require('./routes/contacts');
